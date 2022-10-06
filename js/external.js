6cf9ccc2726ffd6cd4be22627201549b011b38f1
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-10-06 11:35:00 -0500

    adding exercise docs

---
+++ js/external.js
@@ -0,0 +1,36 @@
+"use strict";
+
+console.log('Hello from JavaScript!');
+
+alert('Welcome to my Website!');
+
+let userInput = prompt('What is your favorite color?');
+alert('Great, ' + userInput + ' is my favorite color too!');
+
+let movieRate = 3;
+let movieQty = prompt('How many movies are you renting?');
+let movieDays = prompt('How many days are you renting them for?');
+alert('Your total is $' + (movieRate * movieQty * movieDays) + '.00.');
+
+let GoogPay = 400;
+let AmazPay = 380;
+let FacePay = 350;
+let GoogHour = prompt('How many hours did you work at Google this week?');
+let AmazHour = prompt('How many hours dud you work at Amazon this week?');
+let FaceHour = prompt('How many hours did your work at FaceBook this week?');
+let totalPay = (GoogPay * GoogHour) + (AmazPay * AmazHour) + (FacePay * FaceHour);
+    alert('Your total pay for this week is $' + totalPay + ".00");
+
+let classFull = confirm('Click OK if there is slot available for you in class.  Click Cancel if no.');
+let schConflict = Math.floor(Math.random() *2) === 0;
+if (schConflict === 1 && classFull) {
+    alert('You can enroll in this class.')
+} else alert('You cannot enroll in this class.');
+
+let offerUser = prompt('How many items are you buying?');
+let offerExpired = confirm('Press OK if offer still valid. Click Cancel if no.');
+let offerPremium = confirm('Press OK if Premium Member. Click Cancel if no.');
+if ((offerUser > 2 || offerPremium) && offerExpired) {
+    alert('Product offer can be applied.')
+} else alert('Product offer cannot be applied.');
+
