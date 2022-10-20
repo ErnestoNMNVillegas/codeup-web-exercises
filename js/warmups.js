c3e255d0cbaa0aea22c9a713d58d8e77ed4efe87
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-10-20 09:27:50 -0500

    js warmups

---
+++ js/warmups.js
@@ -0,0 +1,26 @@
+
+console.log('test');
+
+//TODO  Write a function that when passed an object will return the value of the object’s price property.
+//     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+// getPrice(obj); // returns “$7.89”
+
+let objects = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+
+    objects.forEach(function(obj)){
+        Return 'Object price is
+    }
+
+//Correct from walk-thru
+// function getPrice (obj) {
+//     return obj.price;
+// }
+//
+// let potatoes = {
+//     name: "potatoes",
+//     quantity: '2',
+//     price: "7.89"
+// }
+//
+// console.log(getPrice(potatoes));
+
