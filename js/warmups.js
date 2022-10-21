5263d5df575a80cd2c27113d4c512fafe3959636
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-10-21 09:30:56 -0500

    js warm up

--- js/warmups.js
+++ js/warmups.js
@@ -5,11 +5,11 @@ console.log('test');
 //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // getPrice(obj); // returns “$7.89”
 
-let objects = {name: “Potatoes”, quantity: 2, price: “$7.89"};
-
-    objects.forEach(function(obj)){
-        Return 'Object price is
-    }
+// let objects = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+//
+//     objects.forEach(function(obj)){
+//         Return 'Object price is
+//     }
 
 //Correct from walk-thru
 // function getPrice (obj) {
@@ -23,4 +23,26 @@ let objects = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // }
 //
 // console.log(getPrice(potatoes));
+
+//TODO Write a function that when passed an array will return the longest string from the array.
+// Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
+// getLongestString(arr) // Returns “Periwinkle”
+
+let arr= ['green', 'blue', 'Periwinkle'];
+    let legth
+    let longest;
+
+function getLongestString(arr) {
+    for (let i=0; i < arr[i].length; i++){
+        if (arr[i].length > legth) {
+            var length = arr[i].length;
+            longest = arr[i];
+        }
+    }
+    return
+}
+console.log(getLongestString(arr));
+getLongestString(arr);
+
+
 
