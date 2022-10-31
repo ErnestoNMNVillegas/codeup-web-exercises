45131f051aa8f06cb7209b0cd8e143f0a1e1b099
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-10-31 10:46:58 -0500

    flexbox fund exercise

--- js/warmups.js
+++ js/warmups.js
@@ -56,74 +56,74 @@ getLongestString(arr);
 // }
 // console.log(getLongestString(arr));
 
-const hamsters = [
-    {
-        name: "Hamtaro",
-        heightInMM: 86,
-        fur: ['orange', 'white'],
-        gender: "male",
-        dateOfBirth: "August 6"
-    } , {
-        name: "Bijou",
-        heightInMM: 75,
-        fur: ['white'],
-        gender: "female",
-        dateOfBirth: "July 10"
-    } , {
-        name: "Oxnard",
-        heightInMM: 100,
-        fur: ['grey', 'white'],
-        gender: "male",
-        dateOfBirth: "May 3"
-    } , {
-        name: "Boss",
-        heightInMM: 120,
-        fur: ['brown', 'white'],
-        gender: "male",
-        dateOfBirth: "September 21"
-    } , {
-        name: "Snoozer",
-        heightInMM: 85,
-        fur: ['brown', 'white', "pink"],
-        gender: "male",
-        dateOfBirth: "January 14"
-    }
-];
+// const hamsters = [
+//     {
+//         name: "Hamtaro",
+//         heightInMM: 86,
+//         fur: ['orange', 'white'],
+//         gender: "male",
+//         dateOfBirth: "August 6"
+//     } , {
+//         name: "Bijou",
+//         heightInMM: 75,
+//         fur: ['white'],
+//         gender: "female",
+//         dateOfBirth: "July 10"
+//     } , {
+//         name: "Oxnard",
+//         heightInMM: 100,
+//         fur: ['grey', 'white'],
+//         gender: "male",
+//         dateOfBirth: "May 3"
+//     } , {
+//         name: "Boss",
+//         heightInMM: 120,
+//         fur: ['brown', 'white'],
+//         gender: "male",
+//         dateOfBirth: "September 21"
+//     } , {
+//         name: "Snoozer",
+//         heightInMM: 85,
+//         fur: ['brown', 'white', "pink"],
+//         gender: "male",
+//         dateOfBirth: "January 14"
+//     }
+// ];
 
 // getTallestHamster(hamsters); // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
 
-function getTallestHamster (obj) {
-    let heightInMM;
-    let tallest = 0;
-    for (let i=0; i < input.length; i++){
-        if (input[i].length > height){
-            heightInMM = input[i].length;
-            tallest = input[i];
-        }
-    }
-    return tallest;
-    return obj.price;
-}
-
-let potatoes = {
-    name: "potatoes",
-    quantity: '2',
-    price: "7.89"
-}
-
-console.log(getPrice(potatoes));
-
-function getTallestHamster (hamsters) {
-    let height = 0;
-    let tallest = 0;
-    for (let i=0; i < input.length; i++){
-        if (input[i].length > height){
-            height = input[i].length;
-            tallest = input[i];
-        }
-    }
-    return tallest;
-}
+// function getTallestHamster (obj) {
+//     let heightInMM;
+//     let tallest = 0;
+//     for (let i=0; i < input.length; i++){
+//         if (input[i].length > height){
+//             heightInMM = input[i].length;
+//             tallest = input[i];
+//         }
+//     }
+//     return tallest;
+//     return obj.price;
+// }
+//
+// let potatoes = {
+//     name: "potatoes",
+//     quantity: '2',
+//     price: "7.89"
+// }
+//
+// console.log(getPrice(potatoes));
+//
+// function getTallestHamster (hamsters) {
+//     let height = 0;
+//     let tallest = 0;
+//     for (let i=0; i < input.length; i++){
+//         if (input[i].length > height){
+//             height = input[i].length;
+//             tallest = input[i];
+//         }
+//     }
+//     return tallest;
+// }
 
 
 
@@ -136,3 +136,34 @@ function getTallestHamster (hamsters) {
 //     } else console.log(shopper.amount.toFixed(2) + ' is ' + shopper.name + '\'s total.  He got no discount.');
 // });
 
+// TODO Warmup: Write FizzBuzz from 1-100;
+// Numbers evenly divisible by 3 should be replaced by Fizz
+// Numbers evenly divisible by 5 should be replaced by Buzz
+// Numbers evenly divisible by both 3 and 5 should be replaced by FizzBuzz
+// All other numbers should be printed in the console.
+let i = 1
+function fizzBuzz(){
+    do {
+        if (i % 3 === 0) {
+            console.log('Fizz');
+            break;
+        }   else if (i % 5 === 0) {
+            console.log("Buzz");
+            break;
+        }   else if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+            break;
+        }
+
+        // for (let i = 1; i <= 50; i++) {
+        //     if (input === i.toString()) {
+        //         console.log('Yikes!  Skipping number ' + i);
+        //         continue;
+        //     }
+        //     if (i % 2 === 1) {
+        //         console.log('Here is an odd number: ' + i)
+        //     }
+    } while (i =< 100)
+    };
+
+    // Note from walk-thru:  for loop for appropriate.
