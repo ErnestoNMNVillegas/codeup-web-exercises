4e2da5faf80ce6b2249464ca06ed6442acadd29a
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-11-20 19:45:53 -0600

    konami exercise

--- js/jquery.js
+++ js/jquery.js
@@ -30,19 +30,18 @@ $(function() {
 //     $(this).css('color', 'black');
 // })
 
-    ///Konami Code///
+    ///Konami Code Exercise///
 
     $(document).keyup(function(event){
         console.log(event.key);
     });
 
-        var userInput = []
-        var konamiCode = ['ArrowUp','ArrowUp','ArrowDown','ArrowDown','ArrowLeft','ArrowRight','ArrowLeft','ArrowRight', 'b','a','Enter'];
+    var userInput = []
+    var konamiCode = ['ArrowUp','ArrowUp','ArrowDown','ArrowDown','ArrowLeft','ArrowRight','ArrowLeft','ArrowRight', 'b','a','Enter'];
     $(document).keyup(function(e){
             userInput.push(e.key);
             if (userInput.toString().indexOf(konamiCode) >= 0) {
                 //Result if correct code entered.
-                // alert("How do you do, fellow gamer?");
                 cheatCodeActive();
                 //Resets the input array
                 userInput = [];
@@ -51,19 +50,22 @@ $(function() {
         });
 
     function cheatCodeActive() {
-        // <iframe src="https://giphy.com/embed/JTzPN5kkobFv7X0zPJ" width="480" height="269" frameBorder="0"
-        //         className="giphy-embed" allowFullScreen></iframe>
-        // <p><a href="https://giphy.com/gifs/30-rock-steve-buscemi-peacocktv-JTzPN5kkobFv7X0zPJ">via GIPHY</a></p>
+        document.getElementById('howDoYou').style.backgroundImage = "url('img/how-do-you-do-fellow-kids-gif.gif')";
+        tbody.innerHTML = cheatCodeActiveText()
+        alert("You have added 30 lives!!!!  CLICK 'OK'");
+    }
 
-        // $('img.large-magnify-glass-animation').appear(function() {
-        //     $(this).delay(200, function(){
-        //         $(this).attr('src','https://giphy.com/gifs/30-rock-steve-buscemi-peacocktv-JTzPN5kkobFv7X0zPJ');
-        //     });
-        // });
-            // document.body.style.backgroundImage = "url(images/talladegaNights.jpg)";
-            document.body.style.backgroundImage = "url(img/how-do-you-do-fellow-kids-gif.gif)";
-            alert("How do you do, fellow gamer?");
-        }
+    function cheatCodeActiveText() {
+        return '<div>' +
+            '   <h3>How do you do, fellow gamer!?!</h3>' +
+            '   <br>' +
+            '   <h3>I also know the cheat codes!</h3>' +
+            '   <br>' +
+            '   <h3>You have added 30 lives!!</h3>' +
+            '   </div>';
+    }
+
+    var tbody = document.querySelector('#innHtml');
 });
 
 
