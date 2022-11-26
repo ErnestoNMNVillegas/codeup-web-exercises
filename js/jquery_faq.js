33fa2b674d6d217d80f432691ffc03d2a652978a
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-11-25 21:08:00 -0600

    traversing-methods-exercise completion

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,7 +2,7 @@
 
 $(function () {
 
-    $('button')
+    $('#faQ-togg')
         .click(function () {
             $('dd').toggleClass('invisible');
         });
@@ -19,8 +19,32 @@ $(function () {
         $('dd').toggleClass("visible");
     });
 
+    //Transversing Methods
+
+        //Last li highlight
+
+    $('#last-Li').click(function() {
+        $("ul li:last-child").css({"background-color": "yellow"});
+        // $('ul li').last().css('background-color', 'yellow');
 
     });
+
+    // $("#last-Li").click(function(){
+    //     $("ul li").each(function(){
+    //         $(this).last().css('background-color', 'yellow');
+    //     });
+    // });
+
+    $('h3').click(function() {
+        $(this).next().css('font-weight', "bold");
+    });
+
+    $('li').click(function() {
+        $('li:first-child').css('color', "blue");
+    });
+
+
+});
 
 
 
