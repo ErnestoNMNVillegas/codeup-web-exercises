8f8261528a85c8d834a302f05eae71f168f2667a
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-11-28 17:12:55 -0600

    ajax blog  exercise

---
+++ js/ajax-blog.js
@@ -0,0 +1,13 @@
+$.get("data/blog.json").done(function(data) {
+    console.log(data);
+    // $("#insertProducts").append("<tr><td>" + data.title[i] + "</td><td>" + data.quantity[i] + "</td><td>" + data.price[i] + "</td></tr>");
+
+    let blogPosts = '';
+    $.each(data, function(key, value){
+        blogPosts += "<tr><td>" + value.title + "</td><td>" + value.content + "</td><td>" + value.categories + "</td><td>" + value.date + "</td></tr>";
+    })
+
+    // "<p>Request to API complete.</p>"
+
+    $("#posts").append(blogPosts);
+});
