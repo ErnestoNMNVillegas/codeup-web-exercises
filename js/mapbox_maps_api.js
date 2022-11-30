9dd6fcff0609cd69859a95396e202de94d9e2ae4
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-11-30 13:57:48 -0600

    mapbox exercise

--- js/mapbox_maps_api.js
+++
@@ -1,8 +0,0 @@
-//     mapboxgl.accessToken = mapBoxExerciseKey;
-//     var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     zoom: 10,
-//     center: [-98.4916, 29.4252]
-// });
-
