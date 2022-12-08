6ebd53dcb75c905d5d6c58052091af915754d2ee
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-08 11:17:28 -0600

    promises git exercise

--- js/promises.js
+++ js/promises.js
@@ -4,39 +4,34 @@ console.log("test");
 
 (() => {
 
-// fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
-
 //TODO Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
-
+    var tbody = document.getElementById('#last-push');
+    tbody = ""
+    let lastPush = ""
     const lastPushCommit = (userName) => {
-        const myPromise =fetch('https://api.github.com/users/' + userName + '/events/public', {headers: {'Authorization': GITHUB_KEY}})
+        const myPromise = fetch('https://api.github.com/users/' + userName + '/events/public', {headers: {'Authorization': GITHUB_KEY}})
         myPromise.then(response => response.json())
             .then(users => {
                 console.log(users);
-                users.forEach( userObj => {
-                    // do something with each username
-                    console.log(userObj.login);
+                users.forEach(( userObj, index) => {
+                    for (let user of users){
+                        if (userObj.type === "PushEvent"){
+                        // console.log(userObj);
+                            if(index===0){
+                                console.log(userObj.created_at);
+                                tbody.innerHTML = userObj.created_at
+                                return lastPush = userObj.created_at
+                            }
+
+                        }
+                    }
                 })
             })
         myPromise.catch(error => console.error(error));
-
-        // const githubPromise = fetch('https://api.github.com/repositories');
-        // const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
+        console.log(lastPush);
     }
-
+    console.log(lastPush);
     console.log(lastPushCommit("ernestoNMNVillegas"));
-
-    // [1].payload.commits
-
-
-    // Promise.all([githubPromise, bitbucketPromise])
-    //     .then(function(data) {
-    //         // here data is an array of the resolved values from each promise
-    //         // we can now do something with both pieces of data
-    //     })
-    //     .catch(function(error) {
-    //         // handle errors
-    //     });
 
 
 })();
